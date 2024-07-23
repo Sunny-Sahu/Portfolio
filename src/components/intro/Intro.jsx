541eb470fac7d90fefd57419498a0e2e21c5efbc
Sunny Kumar <sraj15050@gmail.com>
2024-07-23 15:32:08 +0530

    Social link added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./intro.css";
+import linkedin from "../../assets/linkedin.webp"
+import git from "../../assets/git.png"
+import naukri from "../../assets/naukri.png"
 
 const Intro = () => {
   return (
@@ -13,6 +16,21 @@ const Intro = () => {
           I'm a skilled web designer with experience in createing <br />{" "}
           visually appealing and user friendly websites
         </p>
+      <div className="container">
+        <div className="smedia">
+          <a href="https://www.linkedin.com/in/sunnysahu15050">
+          <img src={linkedin} alt="git" /></a>
+        </div>
+        <div className="smedia">
+          <a href="https://github.com/Sunny-Sahu">
+          <img src={git} alt="git" /></a>
+        </div>
+        <div className="smedia">
+          <a href="https://www.naukri.com/mnjuser/profile?id=&altresid">
+          <img src={naukri} alt="git" /></a>
+        </div>
+
+      </div>
       </div>
     </section>
   );
